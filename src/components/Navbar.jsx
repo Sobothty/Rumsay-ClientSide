e2812222d625e0201ca7c581e0cd9f584913584d
Sobothty <supercrisz24@gmail.com>
2025-05-01 13:58:38 +0700

    anh tim tver ban mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,21 +4,20 @@ export default function NavbarPage() {
   return (
     <Navbar fluid rounded>
     <NavbarBrand href="/">
-      <img src="src/assets/logo/Rumsay.png" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-      <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+      <img src="src/assets/logo/Rumsay.png" className="ml-1.5 h-14 sm:h-18" alt="Flowbite React Logo" />
     </NavbarBrand>
-    <div className="flex md:order-2">
-      <Button>Get started</Button>
+    <div className="flex gap-2 md:order-2">
+      <Button className="bg-primary">Sign-In</Button>
       <NavbarToggle />
     </div>
     <NavbarCollapse>
-      <NavbarLink href="/" active>
+      <NavbarLink href="/" className="text-white bg-primary font-bold hover:bg-primary md:hover:bg-primary lg:text-primary lg:hover:text-white lg:hover:bg-none">
         Home
       </NavbarLink>
-      <NavbarLink href="/about">About</NavbarLink>
-      <NavbarLink href="#">Services</NavbarLink>
-      <NavbarLink href="#">Pricing</NavbarLink>
-      <NavbarLink href="#">Contact</NavbarLink>
+      <NavbarLink href="/about" className="text-primary font-bold hover:bg-primary">About</NavbarLink>
+      <NavbarLink href="#" className="text-primary font-bold hover:bg-primary">Services</NavbarLink>
+      <NavbarLink href="#" className="text-primary font-bold hover:bg-primary">Pricing</NavbarLink>
+      <NavbarLink href="#" className="text-primary font-bold hover:bg-primary">Contact</NavbarLink>
     </NavbarCollapse>
   </Navbar>
   )
